4c0bd9d9d4d38545b0ccb889ac2a50b41031be04
Dr. Ernesto Lee <socrates73@gmail.com>
2023-07-07 14:04:52 -0700

    Update lesson1_activityA.R

--- lesson1/lesson1_activityA.R
+++ lesson1/lesson1_activityA.R
@@ -1,6 +1,8 @@
 #tested and working
 #load the tidyverse packages
-install.packages("tidyverse")
+# install.packages("tidyverse") # this is pre-installed in most environments but run this if it is not
+
+# data()   # this will show you all of the datasets that are built in.
 
 #load ggplot2
 library(ggplot2)
@@ -11,3 +13,8 @@ str(msleep)
 
 #save a copy of the global environment
 save.image("introDSwR.RData")
+
+# See the packages that are installed
+ip = as.data.frame(installed.packages()[,c(1,3:4)])
+ip = ip[is.na(ip$Priority),1:2,drop=FALSE]
+ip
